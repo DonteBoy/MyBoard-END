51df2968cc7c9a3f506b254f3653dd4dc4161512
DonteBoy <vvlad7381@gmail.com>
2023-08-08 23:12:08 +0300

    .

---
+++ MyBoardEND/main/static/main/js/VANTA.js
@@ -0,0 +1,20 @@
+VANTA.BIRDS({
+  el: ".animated_background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x620,
+  color1: 0xffa400,
+  color2: 0x2837,
+  birdSize: 3.10,
+  wingSpan: 10.00,
+  speedLimit: 4.00,
+  separation: 100.00,
+  alignment: 75.00,
+  cohesion: 50.00,
+  quantity: 3.00
+})
