6c07e1455f4a8b09f426a89319ba18a1dc246586
DonteBoy <vvlad7381@gmail.com>
2023-08-09 15:41:36 +0300

    .

---
+++ MyBoardEND/main/static/main/js/SWAPDOGS.js
@@ -0,0 +1,10 @@
+var sd = new SwapDogs(
+    document.getElementById('header_text_anim'),
+    {
+        autoInit: true,
+        words: ["MyBoard-END", "Index-page", "Good-luck"],
+        letters: 'абвгдеёзжклмнопрст',
+        interval: 10000,
+        interval2: 50
+    }
+);
