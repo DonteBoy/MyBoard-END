c38eb645a5d4af35e1bfc8e8e03f0ceb00b21ffb
DonteBoy <vvlad7381@gmail.com>
2023-08-11 14:33:45 +0300

    .

--- MyBoardEND/main/urls.py
+++ MyBoardEND/main/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('home', views.home)
+    path('', views.index,  name='index'),
+    path('home/', views.home,  name='home'),
+    path('news/', views.home,  name='news'),
+    path('ai_gen/', views.home,  name='ai_gen'),
+    path('3d_room/', views.home,  name='3d_room'),
+    path('honors/', views.home,  name='honors'),
 ]
--- MyBoardEND/main/views.py
+++ MyBoardEND/main/views.py
@@ -4,4 +4,16 @@ def index(request):
     return render(request, "main/index.html")
 
 def home(request):
-    pass
+    return render(request, "main/home.html")
+
+def news(request):
+    return render(request, "main/.html")
+
+def ai_gen(request):
+    return render(request, "main/ai_gen.html")
+
+def three_d_room(request):
+    return render(request, "main/3d_room.html")
+
+def honors(request):
+    return render(request, "main/honors.html")
