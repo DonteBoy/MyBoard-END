ed19190d62f43eb1c6f87ff2fdeae957cd6fbd66
DonteBoy <vvlad7381@gmail.com>
2023-08-12 00:22:01 +0300

    .

--- MyBoardEND/main/views.py
+++ MyBoardEND/main/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, "main/home.html")
 
 def news(request):
-    return render(request, "main/.html")
+    return render(request, "main/news.html")
 
 def ai_gen(request):
     return render(request, "main/ai_gen.html")
